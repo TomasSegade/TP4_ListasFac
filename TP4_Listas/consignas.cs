4a6b92e389fc27fcfde300d42383289d93b2f12e
Tomas Segade <tomy.segade@gmail.com>
2023-09-20 12:51:51 -0300

    Ver hsata aca

---
+++ TP4_Listas/consignas.cs
@@ -0,0 +1,25 @@
+﻿/*
+ 
+ Consignas:
+
+Desarrolle un sistema utilizando una lista que emule la cobranza de una caja de un supermercado.
+
+Los datos de los clientes son:
+
+·        Código.
+
+·        Nombre.
+
+·        Importe.
+
+El sistema debe permitir:
+ 
+·        Agregar un nuevo cliente en la cola de la caja.
+
+·        Cobrar al cliente correspondiente.
+
+·        Mostrar el total de dinero de lo que se va recaudando luego de cobrar a cada cliente.
+
+·        Mostrar un listado con todos los clientes.
+ 
+ */
